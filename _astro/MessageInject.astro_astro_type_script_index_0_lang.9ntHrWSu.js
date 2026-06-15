9e7e54f52fb1f8465c08dfc03b22573cee1d894b
nanarino <nanarino@users.noreply.github.com>
2026-06-15 03:57:36 +0000

    deploy: 20a2fe07d8315bbedbac3b6153e8304b8b31358c

--- _astro/MessageInject.astro_astro_type_script_index_0_lang.9ntHrWSu.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./message.CyguZYKm.js";import"./prefix.DdUm4vFL.js";Reflect.set(window,"message",e);document.addEventListener("astro:after-swap",()=>{e.reset()});
---
+++ _astro/MessageInject.astro_astro_type_script_index_0_lang.BEuXKuhL.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./message.B3FqqM-Q.js";Reflect.set(window,"message",e);document.addEventListener("astro:after-swap",()=>{e.reset()});
